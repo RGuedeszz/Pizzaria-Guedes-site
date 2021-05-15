7e9384bd812ea7d296dbfd4f2ddc5685822adacb
rojao <rlgm872000@gmail.com>
2021-05-15 19:18:20 -0300

    Adicionado informacoes de pizza

---
+++ Pizza.js
@@ -0,0 +1,11 @@
+let pizzas = [
+    {id: 1, name: "calabresa", img: "Pictures/calabresa.png", priceUn: 30.16, sizes: ["100g", "250g", "400g"]},
+    {id: 2, name: "mussarela", img: "Pictures/mussarela.png", priceUn: 21.10, sizes: ["100g", "250g", "380g"]},
+    {id: 3, name: "quatro queijos", img: "Pictures/4 queijos.png", priceUn: 27.16, sizes: ["100g", "250g", "400g"]},
+    {id: 4, name: "camarao e catupiry", img: "Pictures/camarao e catupiry.png", priceUn: 27.80, sizes: ["100g", "250g", "400g"]},
+    {id: 5, name: "portuguesa", img: "Pictures/portuguesa.png", priceUn: 26.16, sizes: ["100g", "250g", "400g"]},
+    {id: 6, name: "americana", img: "Pictures/americana.png", priceUn: 26.10, sizes: ["100g", "250g", "400g"]},
+    {id: 7, name: "presunto e queijo", img: "Pictures/presunto e queijo.png", priceUn: 26.16, sizes: ["100g", "250g", "400g"]},
+    {id: 8, name: "frango e catupiry", img: "Pictures/frango e catupiry.png", priceUn: 32.10, sizes: ["100g", "250g", "400g"]},
+    {id: 9, name: "borda recheada", img: "Pictures/borda.png", priceUn: 10.70, sizes: ["20g", "35g", "50g"]}
+];
